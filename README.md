55cbd6e6411db42edfc62b2cd084306fd4010c53
Chris Voon <Chris.Voon@bertelsmann.de>
2017-07-10 18:21:11 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+Repository to test issue https://github.com/aspnet/Home/issues/1826.
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        private readonly TestServerFixture _fixture;
+        public UnitTest1()
+        {
+            ()_fixture = new TestServerFixture();
+        }
+
+        [Fact]
+        public async Task GetRoot()
+        {
+            var response = await _fixture.Client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal("Value 1", responseString);
+        }
+    }
+}
